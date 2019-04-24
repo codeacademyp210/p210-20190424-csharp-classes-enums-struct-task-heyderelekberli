16e664110cf0c64af8691dc001577eb978af7ae0
heyderelekberli <HeydarRa@code.edu.az>
2019-04-24 20:47:17 +0400

    done

---
+++ homework/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student person = new Student();
+            Console.WriteLine("Please, enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Please, enter your surname");
+            string surname =  Console.ReadLine();
+            Console.WriteLine("Please, enter your group number");
+            string group =  Console.ReadLine();
+            Console.WriteLine($"Salam {name} {surname}. Sizin qrupunuz: {group}");
+            Console.ReadLine();
+        }
+    }
+     
+
+
+
+    class Group
+    {
+        string _Name;
+    }
+    class Student
+    {
+        string _Name;
+        string _Surname;
+        string _Group;
+
+    }
+
+   
+    
+}
